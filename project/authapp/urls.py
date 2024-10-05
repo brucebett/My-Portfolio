2d97e37ad8589b925621a2ebdee324400f34c8b3
brucebett <bett6448@gmail.com>
2024-10-06 01:20:57 +0300

    sun1 commit

--- project/authapp/urls.py
+++ project/authapp/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from portfolio import views
+from authapp import views
 
 urlpatterns = [
     path('signup/', views.signup, name='singup'),
