ff62ad9de513c5f971d391b75426bd988c9b02e4
brucebett <bett6448@gmail.com>
2024-10-08 01:20:51 +0300

    teu commit

--- project/authapp/views.py
+++ project/authapp/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Create your views here.
 def signup(request):
-    # if request.method == "POST":
+    if request.method == "POST":
+        get_email = request.POST.get('email')
+        get_password = request.POST.get('pass1')
+        get_confirm_password = request.POST.get('pass2')
+        if get_password ≠ get_confirm_password:
+            messages.info(request, "Password is not macthing")
+            return redirect('/auth/singup')
+        try:
+            if user.objects.get(username=get_email):
+                messages.warning(request, "Email is taken")
+                return redirect('/auth/signup/')
+        except Exception as identifier:
+            pass
+        myuser=User.objects.create_user(get_email,get_email,get_password)
+        myuser.save()
+        messages.success(request)
 
+    
         
     return render(request, 'signup.html')  
 
