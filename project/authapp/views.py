a5f6190cf2d2759cceb5d02681e72c8be5240c14
brucebett <bett6448@gmail.com>
2024-10-07 23:36:06 +0300

    mon commit

--- project/authapp/views.py
+++ project/authapp/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def signup(request):
+    # if request.method == "POST":
+
+        
     return render(request, 'signup.html')  
 
 def handleLogin(request):
