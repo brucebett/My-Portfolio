489b6d54fd43a90d659bd673a366250cb27d0533
brucebett <bett6448@gmail.com>
2024-11-02 23:43:58 +0300

    sat EVEn commit

--- project/portfolio/models.py
+++ project/portfolio/models.py
@@ -10,3 +10,15 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Blogs(models.Model):
+    tittle=models.CharField(max_length=60)
+    description=models.TextField()
+    authname=models.CharField(max_length=15)
+    img=models.ImageField(upload_to='blog', blank=True,null=True)
+    timeStamp=models.DateTimeField(auto_now_add=True, blank=True)
+
+
+    def __str__(self):
+        return self.tittle
