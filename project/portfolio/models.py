ea4e73954d50604e9d0d600ed6d92c476d503174
brucebett <bett6448@gmail.com>
2024-11-11 21:04:58 +0300

    mon1 commit

--- project/portfolio/models.py
+++ project/portfolio/models.py
@@ -22,3 +22,19 @@ class Blogs(models.Model):
 
     def __str__(self):
         return self.tittle
+    
+
+class Internship(models.Model):
+    fullname=models.CharField(max_length=60)
+    usn=models.CharField(max_length=60)
+    email=models.EmailField(max_length=60)
+    college_name=models.CharField(max_length=100)
+    offer_status=models.CharField(max_length=60)
+    start_date=models.CharField(max_length=60)
+    end_date=models.CharField(max_length=60)
+    project_report=models.CharField(max_length=60)
+    timeStamp=models.DateTimeField(auto_now_add=True, blank=True)
+
+
+    def __str__(self):
+        return self.usn
